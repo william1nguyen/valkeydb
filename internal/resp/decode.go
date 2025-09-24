fb3d61fc9a7a6f10b053b1a41b10c97eb93ce58f
william1nguyen <vietanhhd03@gmail.com>
2025-09-24 18:59:32 +0700

    fix(resp): refactor decode and encode

--- internal/resp/decode.go
+++ internal/resp/decode.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strconv"
-
-	"github.com/william1nguyen/valkeydb/internal/utils"
+	"strings"
 )
 
 func Decode(r *bufio.Reader) (Value, error) {
@@ -17,73 +16,103 @@ func Decode(r *bufio.Reader) (Value, error) {
 
 	switch prefix {
 	case '+':
-		line, _ := utils.Readline(r)
-		return Value{
-			Type: STRING,
-			Str:  line,
-		}, nil
-
+		return readString(r)
 	case '-':
-		line, _ := utils.Readline(r)
-		return Value{
-			Type: ERROR,
-			Str:  line,
-		}, nil
-
+		return readError(r)
 	case ':':
-		line, _ := utils.Readline(r)
-		n, err := strconv.ParseInt(line, 10, 64)
-		if err != nil {
-			return Value{}, err
-		}
-		return Value{
-			Type: INT,
-			Int:  n,
-		}, nil
-
+		return readInt(r)
 	case '$':
-		line, _ := utils.Readline(r)
-		length, _ := strconv.Atoi(line)
-		if length == -1 {
-			return Value{
-				Type:  BULKSTRING,
-				IsNil: true,
-			}, nil
-		}
-
-		buf := make([]byte, length+2)
-		if _, err := io.ReadFull(r, buf); err != nil {
-			return Value{}, err
-		}
-		return Value{
-			Type: BULKSTRING,
-			Str:  string(buf[:length]),
-		}, nil
+		return readBulkString(r)
 	case '*':
-		line, _ := utils.Readline(r)
-		count, _ := strconv.Atoi(line)
-
-		if count == -1 {
-			return Value{
-				Type:  ARRAY,
-				IsNil: true,
-			}, nil
-		}
-
-		items := make([]Value, 0, count)
-		for i := 0; i < count; i++ {
-			val, err := Decode(r)
-			if err != nil {
-				return Value{}, err
-			}
-			items = append(items, val)
-		}
-
-		return Value{
-			Type:  ARRAY,
-			Array: items,
-		}, nil
+		return readArray(r)
 	}
 
 	return Value{}, fmt.Errorf("unknown RESP type: %q", prefix)
+}
+
+func readline(r *bufio.Reader) (string, error) {
+	line, err := r.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSuffix(
+		strings.TrimSuffix(line, "\n"),
+		"\r",
+	), nil
+}
+
+func readString(r *bufio.Reader) (Value, error) {
+	line, _ := readline(r)
+	return Value{
+		Type: STRING,
+		Str:  line,
+	}, nil
+}
+
+func readError(r *bufio.Reader) (Value, error) {
+	line, _ := readline(r)
+	return Value{
+		Type: ERROR,
+		Str:  line,
+	}, nil
+}
+
+func readInt(r *bufio.Reader) (Value, error) {
+	line, _ := readline(r)
+	n, err := strconv.ParseInt(line, 10, 64)
+	if err != nil {
+		return Value{}, err
+	}
+	return Value{
+		Type: INT,
+		Int:  n,
+	}, nil
+}
+
+func readBulkString(r *bufio.Reader) (Value, error) {
+	line, _ := readline(r)
+	length, _ := strconv.Atoi(line)
+	if length == -1 {
+		return Value{
+			Type:  BULKSTRING,
+			IsNil: true,
+		}, nil
+	}
+
+	buf := make([]byte, length+2)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return Value{}, err
+	}
+	return Value{
+		Type: BULKSTRING,
+		Str:  string(buf[:length]),
+	}, nil
+}
+
+func readArray(r *bufio.Reader) (Value, error) {
+	line, _ := readline(r)
+	count, _ := strconv.Atoi(line)
+
+	if count == -1 {
+		return Value{
+			Type:  ARRAY,
+			IsNil: true,
+		}, nil
+	}
+
+	items := make([]Value, 0, count)
+	for i := 0; i < count; i++ {
+		val, err := Decode(r)
+		if err != nil {
+			return Value{}, err
+		}
+		items = append(items, val)
+	}
+
+	return Value{
+		Type:  ARRAY,
+		Array: items,
+	}, nil
 }
--- internal/resp/encode.go
+++ internal/resp/encode.go
@@ -8,26 +8,47 @@ import (
 func Encode(v Value) string {
 	switch v.Type {
 	case STRING:
-		return fmt.Sprintf("+%s\r\n", v.Str)
+		return encodeString(v)
 	case ERROR:
-		return fmt.Sprintf("-%s\r\n", v.Str)
+		return encodeError(v)
 	case INT:
-		return fmt.Sprintf(":%s\r\n", strconv.FormatInt(v.Int, 10))
+		return encodeInt(v)
 	case BULKSTRING:
-		if v.IsNil {
-			return "$-1\r\n"
-		}
-		return fmt.Sprintf("$%d\r\n%s\r\n", len(v.Str), v.Str)
+		return encodeBulkString(v)
 	case ARRAY:
-		if v.IsNil {
-			return "*-1\r\n"
-		}
-		out := fmt.Sprintf("*%d\r\n", len(v.Array))
-		for _, element := range v.Array {
-			out += Encode(element)
-		}
-		return out
+		return encodeArray(v)
 	}
 
 	return ""
+}
+
+func encodeString(v Value) string {
+	return fmt.Sprintf("+%s\r\n", v.Str)
+}
+
+func encodeError(v Value) string {
+	return fmt.Sprintf("-%s\r\n", v.Str)
+}
+
+func encodeInt(v Value) string {
+	return fmt.Sprintf(":%s\r\n", strconv.FormatInt(v.Int, 10))
+}
+
+func encodeBulkString(v Value) string {
+	if v.IsNil {
+		return "$-1\r\n"
+	}
+
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(v.Str), v.Str)
+}
+
+func encodeArray(v Value) string {
+	if v.IsNil {
+		return "*-1\r\n"
+	}
+	out := fmt.Sprintf("*%d\r\n", len(v.Array))
+	for _, element := range v.Array {
+		out += Encode(element)
+	}
+	return out
 }
